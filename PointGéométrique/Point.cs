19529e8bd6cee4230bd5788e24beb2efd8cba46e
RihamBouchikhi1 <riham.bouchikhi@usmba.ac.ma>
2022-11-10 22:06:41 +0100

    modify

--- PointGéométrique/Point.cs
+++ PointGéométrique/Point.cs
@@ -30,21 +30,78 @@ namespace PointGéométrique
         {
             return y;
         }
-        void translation(float N)
+        public static void translation(Point P,float N)
         {
-            x += N;
-            y += N;
+            P.x += N;
+            P.y += N;
         }
-        void Egale(Point p)
+        public static void Egale(Point p1, Point p2)
         {
-            if (x == p.x && y == p.y)
+            if (p1.x == p2.x && p1.y == p2.y)
                 Console.Write("les deux points sont égaux");
             else
                 Console.Write("les deux points ne sont pas égaux");
         }
-        double distance(Point p)
+        public static double distance(Point p1, Point p2)
         {
-            return Math.Sqrt(Math.Pow(x - p.x, 2) +Math.Pow(y - p.y, 2));
+            return Math.Sqrt(Math.Pow(p1.x - p2.x, 2) +Math.Pow(p1.y - p2.y, 2));
+        }
+        public static void Linearite(Point A, Point B, Point C)
+        {
+            double a, b;
+            if (A.x != B.x)
+            {
+                a = (A.y - B.y) / (A.x - B.x);
+                b = A.y + a * A.x;   
+                if (a * C.x + b == C.y)
+                {
+                    Console.WriteLine("les trois points sont lineaire");
+                }
+                else
+                    Console.WriteLine("les trois points ne sont pas lineaire");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Point P1 = new Point();
+            Point P2 = new Point();
+            Point P3 = new Point();
+
+
+            Console.WriteLine("Entrez l'abscisse de 1ere point");
+            float Xp1 = int.Parse(Console.ReadLine());
+            P1.set_x(Xp1);
+
+            Console.WriteLine("Entrez l'ordonnee de 1ere point");
+            float Yp1 = int.Parse(Console.ReadLine());
+            P1.set_y(Yp1);
+
+            Console.WriteLine("Entrez l'abscisse de 2eme point");
+            float Xp2 = int.Parse(Console.ReadLine());
+            P2.set_x(Xp2);
+
+            Console.WriteLine("Entrez l'ordonnee de 2eme point");
+            float Yp2 = int.Parse(Console.ReadLine());
+            P2.set_y(Yp2);
+
+            Console.WriteLine("Entrez l'abscisse de 3eme point");
+            float Xp3 = int.Parse(Console.ReadLine());
+            P3.set_x(Xp3);
+
+            Console.WriteLine("Entrez l'ordonnee de 3eme point");
+            float Yp3 = int.Parse(Console.ReadLine());
+            P3.set_y(Yp3);
+            //______Distance_____//
+            Console.WriteLine("la distance entre ces deux points est :" + distance(P1, P2));
+            //____Egalité____//
+            //Egale(P1, P2);
+            //_____Translation___//
+            //translation(P3,5);
+            //____Linéarité____//
+            //Linearite(P1, P2, P3);
+
+
         }
     }
 }
--- PointGéométrique/Program.cs
+++ PointGéométrique/Program.cs
@@ -10,8 +10,46 @@ namespace PointGéométrique
     {
         static void Main(string[] args)
         {
+            Point P1 = new Point();
+            Point P2 = new Point();
+            Point P3 = new Point();
+
+
+            Console.WriteLine("Entrez l'abscisse de 1ere point");
+            float Xp1 = int.Parse(Console.ReadLine());
+            P1.set_x(Xp1);
+
+            Console.WriteLine("Entrez l'ordonnee de 1ere point");
+            float Yp1 = int.Parse(Console.ReadLine());
+            P1.set_y(Yp1);
+
+            Console.WriteLine("Entrez l'abscisse de 2eme point");
+            float Xp2 = int.Parse(Console.ReadLine());
+            P2.set_x(Xp2);
+
+            Console.WriteLine("Entrez l'ordonnee de 2eme point");
+            float Yp2 = int.Parse(Console.ReadLine());
+            P2.set_y(Yp2);
+
+            Console.WriteLine("Entrez l'abscisse de 3eme point");
+            float Xp3 = int.Parse(Console.ReadLine());
+            P3.set_x(Xp3);
+
+            Console.WriteLine("Entrez l'ordonnee de 3eme point");
+            float Yp3 = int.Parse(Console.ReadLine());
+            P3.set_y(Yp3);
+            //______Distance_____//
+             Console.WriteLine("la distance entre ces deux points est :" + Distance(P1, P2));
+            //____Egalité____//
+            Egale(P1, P2);
+            //_____Translation___//
+            Translation(P1);
+            //____Linéarité____//
+             Linearite(P1, P2, P3);
+
+
         }
- 
+
 
     }
 }
